8abab812c284974d89d7ac1034ac6e961a33582b
unu <unumin@gmail.com>
2013-09-03 21:44:44 +0200

    updated test runner

--- test/run.php
+++ test/run.php
@@ -7,39 +7,32 @@
  * 1: failed, 2: skipped, 3: passed
  */
 header("Content-Type: text/plain");
-$pass = !!$fail = $skip = array();
+$pass = $fail = $skip = array();
 chdir(isset($argv[1]) ? $argv[1] : __DIR__);
 foreach (glob("test.*.php") as $file){
-  if ((realpath($file)) == __FILE__) continue;
-  $name = preg_replace('~test\.(.*)\.php~', '\1', $file);
+  $name = basename($file, '.php');
   if (!is_dir("expect")) mkdir("expect", 0755);
-  if (!is_file("expect/$file.txt")) touch("expect/$file.txt");
+  if (!is_file("expect/$name.txt")) touch("expect/$name.txt");
   $out = array();
   exec('php -f ' . escapeshellarg($file) . ' 2>&1', $out, $exit);
-  $out = implode("\n", $out) . "\n";
   if ($exit == 2)
     $skip[] = $name . !print('s');
-  elseif ($exit == 3 || ($exit != 1
-      && $out === file_get_contents("expect/$file.txt")))
-    $pass += print('.');
+  elseif ($exit == 3 || (!$exit && implode("\n", $out) . "\n"
+      === file_get_contents("expect/$name.txt")))
+    $pass[] = print('.');
   else{
     $fail[] = $name . !print('f');
-    file_put_contents("$file.txt", $out);
-    shell_exec('diff -u -- ' . escapeshellarg("expect/$file.txt")
-      . ' ' .  escapeshellarg("$file.txt") . ' > '
-      . escapeshellarg("$file.diff")
-    );
+    file_put_contents("$name.txt", implode("\n", $out) . "\n");
+    shell_exec('diff -u -- ' . escapeshellarg("expect/$name.txt") . ' '
+      .  escapeshellarg("$name.txt") . ' > ' . escapeshellarg("$name.diff"));
     continue;
   }
-  if (is_file("$file.txt")) unlink("$file.txt");
-  if (is_file("$file.diff")) unlink("$file.diff");
+  if (is_file("$name.txt")) unlink("$name.txt");
+  if (is_file("$name.diff")) unlink("$name.diff");
 }
-if (count($skip))
-  echo "\nSKIPPED: " . implode(', ', $skip);
-if (count($fail))
-  echo "\nFAILED: " . implode(', ', $fail);
-printf("\nTOTAL %d:%d%s (%.0Fms)\n",
-  $pass, count($fail),
+if (count($skip)) echo "\nSKIPPED: " . implode(', ', $skip);
+if (count($fail)) echo "\nFAILED: " . implode(', ', $fail);
+printf("\nTOTAL %d:%d%s (%.0Fms)\n", count($pass), count($fail),
   count($skip) ? " (" . count($skip) . " skipped)" : '',
   (microtime(true) - $_SERVER['REQUEST_TIME_FLOAT']) * 1000);
 exit($fail ? 1 : 0);
